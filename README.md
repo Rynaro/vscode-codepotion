10e0685450979c6cabc05ed0f39a32a68eed54de
Henrique Aparecido Lavezzo <henlavezzo@gmail.com>
2018-06-12 00:21:10 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal alchemy board
+
+Here you'll find my VSCode preferred ingredients
---
+++ configure.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import os
+from shutil import copy
+import glob
+import platform
+import subprocess
+
+EXT_PACKAGES = [
+  '2gua.rainbow-brackets',
+  'PKief.material-icon-theme',
+  'dracula-theme.theme-dracula',
+  'eamodio.gitlens',
+  'octref.vetur',
+  'oderwat.indent-rainbow',
+  'riccardoNovaglia.missinglineendoffile',
+  'shardulm94.trailing-spaces'
+]
+
+def check_vscode_existence():
+  try:
+    subprocess.call(['which', 'code'])
+    return True
+  except:
+    return False
+
+def install_extension(extension):
+  subprocess.call(['code', '--install-extension', extension])
+
+def prepare_folder(path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+
+def copy_files(from_location, to_location):
+  for file in glob.glob(from_location):
+    print('Copying {}...'.format(file))
+    copy(file, to_location)
+
+def install_font():
+  prepare_folder('~/.fonts')
+  copy_files('./fonts/*.ttf', '~/.fonts/')
+  # subprocess.call(['fc-cache', '-f', '-v'])
+
+def configure_editor_preferences():
+  prepare_folder('~/.config/Code/User')
+  copy_files('./preferences/*.json', '~/.config/Code/User')
+
+def main():
+  if check_vscode_existence():
+    print('Installing extensions...')
+    for extension in EXT_PACKAGES:
+      install_extension(extension)
+    print('Installing fonts...')
+    install_font()
+    print('Sending profile preferences...')
+    configure_editor_preferences()
+    print('--------------')
+    print('VSCode ready!')
+    print('--------------')
+  else:
+    print('Please install VSCode, getting from: https://code.visualstudio.com')
+
+if __name__ == '__main__':
+  main()
